e1c19a5ec19fb26905145d524b971cf33241033b
Tiparium <merjel.twikens@gmail.com>
2022-09-28 09:57:12 -0700

    added three sliders, which control window background color

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -14,8 +14,6 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    // In your constructor, you should add any child components, and
-    // initialise any special settings that your component needs.
 
 }
 
@@ -25,15 +23,69 @@ MainComponent::~MainComponent()
 
 void MainComponent::paint (juce::Graphics& g)
 {
-    setBoundsRelative(0.35f, 0.35f, 0.5f, 0.5f);
+    setSize(600, 400);
+    size();
     g.drawRect(getLocalBounds(), 1);   // draw an outline around the component
-    g.fillAll (juce::Colours::darkgoldenrod);
+    g.fillAll (juce::Colour(_rVal, _gVal, _bVal));
 
     g.setColour (juce::Colours::grey);
 
+    // Red Slider
+    addAndMakeVisible(_rSlider);
+    _rSlider.setSliderStyle(juce::Slider::LinearHorizontal);
+    _rSlider.setRange(0, 255, 1);
+    _rSlider.setTextValueSuffix(" R");
+    
+    addAndMakeVisible(_rLabel);
+    _rLabel.setText("RED", juce::NotificationType::dontSendNotification);
+    _rLabel.attachToComponent(&_rSlider, true);
+    _rSlider.addListener(this);
+    // *Red Slider
 
+    // Green Slider
+    addAndMakeVisible(_gSlider);
+    _gSlider.setSliderStyle(juce::Slider::LinearHorizontal);
+    _gSlider.setRange(0, 255, 1);
+    _gSlider.setTextValueSuffix(" G");
 
-    g.setColour (juce::Colours::white);
+    addAndMakeVisible(_gLabel);
+    _gLabel.setText("GREEN", juce::NotificationType::dontSendNotification);
+    _gLabel.attachToComponent(&_gSlider, true);
+    _gSlider.addListener(this);
+    // *Green Slider
+
+    // Blue Slider
+    addAndMakeVisible(_bSlider);
+    _bSlider.setSliderStyle(juce::Slider::LinearHorizontal);
+    _bSlider.setRange(0, 255, 1);
+    _bSlider.setTextValueSuffix(" B");
+
+    addAndMakeVisible(_bLabel);
+    _bLabel.setText("BLUE", juce::NotificationType::dontSendNotification);
+    _bLabel.attachToComponent(&_bSlider, true);
+    _bSlider.addListener(this);
+    // *Blue Slider
+
+    if (_gVal > 180 || (_gVal > 150 && _bVal > 150)) {
+        g.setColour(juce::Colours::black);
+
+        _rLabel.setColour(juce::Label::textColourId, juce::Colours::black);
+        _rSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::black);
+        _gLabel.setColour(juce::Label::textColourId, juce::Colours::black);
+        _gSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::black);
+        _bLabel.setColour(juce::Label::textColourId, juce::Colours::black);
+        _bSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::black);
+    }
+    else {
+        g.setColour(juce::Colours::white);
+        _rLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+        _rSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::white);
+        _gLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+        _gSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::white);
+        _bLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+        _bSlider.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::white);
+    }
+
     g.setFont (30.0f);
     g.drawText (currentSizeAsString, getLocalBounds(),
                 juce::Justification::centred, true);
@@ -41,6 +93,31 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    setBoundsRelative(0.35f, 0.35f, 0.3f, 0.3f);
+    size();
     currentSizeAsString = juce::String (getWidth()) + " x " + juce::String (getHeight());
+
+    int height = getHeight() - getHeight() / 1.5;
+
+    const int border = 100;
+    // Resize Sliders
+    _rSlider.setBounds(border, height, getWidth() - border, 20);
+    _gSlider.setBounds(border, height + 20, getWidth() - border, 20);
+    _bSlider.setBounds(border, height + 40, getWidth() - border, 20);
+    // *Resize Sliders
+}
+
+void MainComponent::size() {
+    setBoundsRelative(0.0f, 0.0f, 1.0f, 1.0f);
+}
+
+void MainComponent::sliderValueChanged(juce::Slider* slider) {
+    if (slider == &_rSlider) {
+        _rVal = _rSlider.getValue();
+    }
+    else if (slider == &_gSlider) {
+        _gVal = _gSlider.getValue();
+    }
+    else {
+        _bVal = _bSlider.getValue();
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -15,7 +15,7 @@
 //==============================================================================
 /*
 */
-class MainComponent  : public juce::Component
+class MainComponent : public juce::Component, public juce::Slider::Listener
 {
 public:
     MainComponent();
@@ -23,8 +23,23 @@ public:
 
     void paint (juce::Graphics&) override;
     void resized() override;
+    void size();
+    void sliderValueChanged(juce::Slider* slider) override;
 
 private:
     juce::String currentSizeAsString;
+
+    juce::Label _rLabel;
+    juce::Label _gLabel;
+    juce::Label _bLabel;
+
+    juce::Slider _rSlider;
+    juce::Slider _gSlider;
+    juce::Slider _bSlider;
+
+    juce::uint8 _rVal = 0;
+    juce::uint8 _gVal = 0;
+    juce::uint8 _bVal = 0;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
